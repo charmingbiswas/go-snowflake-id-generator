a8ffc6e30fe30f134cead21c1a80a96993353595
Riyansh Biswas <charmingbiswas@gmail.com>
2026-04-10 18:36:13 +0530

    added graceful shutdown for go server

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/charmingbiswas/go-snowflake-id-generator/pkg/snowflake"
 )
 
 func main() {
+	var wg sync.WaitGroup
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: nil,
+	}
+
 	machineId, err := snowflake.GetMachineIdFromEnv()
 	if err != nil {
 		log.Fatalf("invalid machine id %v", err)
@@ -22,10 +32,14 @@ func main() {
 	}
 
 	http.HandleFunc("GET /snowflake/health", func(w http.ResponseWriter, r *http.Request) {
+		wg.Add(1)
+		defer wg.Done()
 		w.WriteHeader(http.StatusOK)
 	})
 
 	http.HandleFunc("GET /snowflake/generate", func(w http.ResponseWriter, r *http.Request) {
+		wg.Add(1)
+		defer wg.Done()
 		id, err := node.GenerateSnowflakeId()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -41,6 +55,26 @@ func main() {
 		})
 	})
 
-	log.Println("Starting server on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	go func() {
+		fmt.Println("Starting server on port 8000")
+		fmt.Println(server.ListenAndServe())
+	}()
+
+	shutdownCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+	<-shutdownCtx.Done()
+
+	fmt.Println("Gracefully shutting down server...")
+	ctx, close := context.WithTimeout(context.Background(), 5*time.Second)
+	defer close()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown with error %v", err)
+	}
+
+	fmt.Println("Server shutdown complete")
+
+	fmt.Println("Waiting for go routines to shut down")
+	wg.Wait()
+	fmt.Println("All go-routines stopped. Graceful shutdown complete.")
 }
